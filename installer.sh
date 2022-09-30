820a1b871a9f3803521154518ee7cf8637db2074
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2022-09-30 16:53:03 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,3 @@
-curl -LO
+curl -LO https://raw.githubusercontent.com/Kaiserrrrrr/replit-djs-starter/main/index.js
 curl "https://up.repl.link/add?repl=${REPL_SLUG}&author=${REPL_OWNER}"
 rm installer.sh
